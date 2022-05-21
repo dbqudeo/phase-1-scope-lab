d8c253b2a1f0344c5d60186b777eb81986b71d39
byeongdae yu <brianbdyu@gmail.com>
2022-05-21 10:00:25 -0400

    commit tree c28f936edce517e65dd6a6aa45211e0da96df0b1

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+var bestCustomer;
+function setBestCustomer(){
+     bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'ted';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bd';
+}
+/*
+  
+  describe('setBestCustomer()', function() {
+    it('setBestCustomer', function() {
+      expect(window.bestCustomer).to.equal(undefined);
+
+      setBestCustomer();
+
+      expect(window.bestCustomer).to.equal('not bob');
+    });
+  });
+
+  describe('overwriteBestCustomer()', function() {
+    it('overwrites the best customer', function() {
+      overwriteBestCustomer('maybe bob');
+
+      expect(window.bestCustomer).to.equal('maybe bob');
+    });
+  });
+
+  describe('changeLeastFavoriteCustomer()', function() {
+    it('unsuccessfully tries to reassign the least favorite customer', function() {
+      expect(changeLeastFavoriteCustomer).to.throw('Assignment to constant variable.');
+    });
+  });
+});
+*/
